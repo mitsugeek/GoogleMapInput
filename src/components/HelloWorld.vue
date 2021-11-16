c7bf7d9ccaf339b2c9cbde13f0a68d42e28c1b7e
mitsugeek <mitsu322@gmail.com>
2021-11-16 16:43:37 +0900

    google map sample

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,36 +5,25 @@ defineProps({
   msg: String
 })
 
-const count = ref(0)
+const address = ref(null);
+const map =  ref(null);
+
+const mapSearch = function(){
+  console.log("mapSearch");
+  map.value = address.value;
+};
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div>
+    <input type="text" v-model="address" />
+    <button v-on:click="mapSearch">検索</button>
+  </div>
+  <div>
+  <iframe :src="'https://maps.google.co.jp/maps?output=embed&q=' + map" style="width:100%; height:80vh;"></iframe>
+  </div>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
 </style>
